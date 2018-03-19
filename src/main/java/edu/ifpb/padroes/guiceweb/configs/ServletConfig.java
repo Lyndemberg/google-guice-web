866f2840394d6da7658d404c4a27a0c798017a1d
Lyndemberg <lyndembergbatista@outlook.com>
2018-03-19 18:22:11 -0300

    subindo projeto interminado, ainda

---
+++ src/main/java/edu/ifpb/padroes/guiceweb/configs/ServletConfig.java
@@ -0,0 +1,26 @@
+package edu.ifpb.padroes.guiceweb.configs;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+import com.google.inject.servlet.ServletModule;
+import edu.ifpb.padroes.guiceweb.controllers.FrontController;
+
+/**
+ *
+ * @author lyndemberg
+ */
+public class ServletConfig extends GuiceServletContextListener{
+
+    @Override
+    protected Injector getInjector() {
+        
+        return Guice.createInjector(new ServletModule(){
+            @Override
+            protected void configureServlets(){
+                serve("/*").with(FrontController.class);
+            }
+        });
+    }
+    
+}
